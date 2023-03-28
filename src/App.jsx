8fee4db8605a28045eff56c37cfdba6b6e4c94cb
Nicholas Mendez <nicholasmeeendez@gmail.com>
2023-03-28 09:13:33 -0400

    Uploading

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Table } from './components/Table'
+function App() {
+
+
+  return (
+    <div className="">
+      <Table/>
+    </div>
+  )
+}
+
+export default App
